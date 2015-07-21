c74d4576705ca411c28e9ca8021a1964921f8f38
ray <always_0122@163.com>
2015-07-21 18:36:35 +0800

    增加聊天设置UI

--- app/src/main/java/com/tongban/im/activity/ChatSettingsActivity.java
+++ app/src/main/java/com/tongban/im/activity/ChatSettingsActivity.java
@@ -1,8 +1,16 @@
 package com.tongban.im.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.tongban.corelib.utils.ToastUtil;
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
 import com.tongban.im.adapter.MemberGridAdapter;
@@ -15,9 +23,18 @@ import java.util.List;
 /**
  * 聊天设置
  */
-public class ChatSettingsActivity extends BaseToolBarActivity {
+public class ChatSettingsActivity extends BaseToolBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private GridView gvMembers;
+    //圈子信息
+    private TextView tvGroupName, tvAddress, tvCreator, tvAttrs, tvDesc;
+    private ImageView ivCreator;
+    //圈子设置
+    private CheckBox chbRemind, chbTop;
+    //各条目父控件
+    private View vGroupName, vAddress, vDesc, vClear, vInform;
+    private Button btnQuit;
+
     private MemberGridAdapter mMemberGridAdapter;
     private Group group;
 
@@ -33,16 +50,49 @@ public class ChatSettingsActivity extends BaseToolBarActivity {
 
     @Override
     protected void initView() {
+
         gvMembers = (GridView) findViewById(R.id.gv_members);
+
+        tvGroupName = (TextView) findViewById(R.id.tv_group_name);
+        tvAddress = (TextView) findViewById(R.id.tv_group_address);
+        tvCreator = (TextView) findViewById(R.id.tv_group_creator);
+        tvAttrs = (TextView) findViewById(R.id.tv_group_attrs);
+        tvDesc = (TextView) findViewById(R.id.tv_group_desc);
+
+        ivCreator = (ImageView) findViewById(R.id.iv_group_creator);
+
+        chbRemind = (CheckBox) findViewById(R.id.chb_remind);
+        chbTop = (CheckBox) findViewById(R.id.chb_top);
+
+        vGroupName = findViewById(R.id.ll_name);
+        vAddress = findViewById(R.id.ll_address);
+        vDesc = findViewById(R.id.ll_desc);
+        vClear = findViewById(R.id.ll_clear);
+        vInform = findViewById(R.id.ll_inform);
+
+        btnQuit = (Button) findViewById(R.id.btn_quit);
     }
 
     @Override
     protected void initListener() {
 
+        gvMembers.setOnItemClickListener(this);
+        btnQuit.setOnClickListener(this);
+
+        vGroupName.setOnClickListener(this);
+        vAddress.setOnClickListener(this);
+        vDesc.setOnClickListener(this);
+        vClear.setOnClickListener(this);
+        vInform.setOnClickListener(this);
+
     }
 
     @Override
     protected void initData() {
+
+        Glide.with(ChatSettingsActivity.this).
+                load("http://www.qjis.com/uploads/allimg/120918/11305V125-21.jpg").into(ivCreator);
+
         List<User> users = new ArrayList<>();
         int i = 0;
         while (true) {
@@ -62,4 +112,15 @@ public class ChatSettingsActivity extends BaseToolBarActivity {
         gvMembers.setAdapter(mMemberGridAdapter);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btnQuit) {
+            ToastUtil.getInstance(mContext).showToast("就不退出");
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
