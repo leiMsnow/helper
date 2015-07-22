80dff325328f89cab72c93148f29288487449695
fushudi <fushudi@dangdang.com>
2015-07-22 18:05:56 +0800

    删除无用资源

---
+++ app/src/main/java/com/tongban/im/fragment/DiscoverFragment.java
@@ -0,0 +1,34 @@
+package com.tongban.im.fragment;
+
+
+import com.tongban.corelib.base.fragment.BaseApiFragment;
+import com.tongban.im.R;
+
+/**
+ * 发现页
+ * author: chenenyu 15/7/13
+ */
+public class DiscoverFragment extends BaseApiFragment {
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.fragment_discover;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+
+}
