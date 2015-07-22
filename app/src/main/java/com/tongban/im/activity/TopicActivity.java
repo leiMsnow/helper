09b8cb7ccba03bfb7a5d657210660e862ef43dd9
fushudi <fushudi@dangdang.com>
2015-07-22 16:36:37 +0800

    修改话题列表UI-动画

--- app/src/main/java/com/tongban/im/activity/TopicActivity.java
+++ app/src/main/java/com/tongban/im/activity/TopicActivity.java
@@ -1,16 +1,17 @@
 package com.tongban.im.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.tongban.corelib.utils.ScreenUtils;
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
 import com.tongban.im.adapter.TopicAdapter;
@@ -32,6 +33,8 @@ public class TopicActivity extends BaseToolBarActivity implements AbsListView.On
     private TextView tvTopicByMe;
     private View moveLine;
     private int screenWidth;
+    private float moveLineFrom;
+    private float moveLineTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,7 @@ public class TopicActivity extends BaseToolBarActivity implements AbsListView.On
         mListView = (ListView) findViewById(R.id.lv_chat_list);
         tvTopicByHot = (TextView) findViewById(R.id.tv_topic_by_hot);
         tvTopicByMe = (TextView) findViewById(R.id.tv_topic_by_me);
-        // moveLine = findViewById(R.id.move_line);
+        moveLine = findViewById(R.id.move_line);
     }
 
     @Override
@@ -61,11 +64,13 @@ public class TopicActivity extends BaseToolBarActivity implements AbsListView.On
     @Override
     protected void initData() {
 
-//        screenWidth = getWindowManager().getDefaultDisplay().getWidth();
-//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        params.width = screenWidth / 2;
-//        params.height=4;
-//        moveLine.setLayoutParams(params);
+        screenWidth = ScreenUtils.getScreenWidth(mContext);
+        ViewGroup.LayoutParams params = moveLine.getLayoutParams();
+        params.width = screenWidth / 2;
+        params.height = 4;
+
+        moveLine.setLayoutParams(params);
+
 
         List<Topic> listsByHot = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -111,31 +116,40 @@ public class TopicActivity extends BaseToolBarActivity implements AbsListView.On
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_topic_by_hot:
-                mAdapter.clear();
-                for (int i = 0; i < 10; i++) {
-                    Topic topic = new Topic();
-                    topic.setTopicContent("RayRay的爸爸：#食物中含有硫酸锌？酸奶？#" + i);
-                    topic.setTopicName("什么食物中含有硫酸锌？" + i);
-                    topic.setTopicReplyNum(String.valueOf(i));
-                    mAdapter.add(topic);
-                }
-                tvTopicByHot.setSelected(true);
-                tvTopicByMe.setSelected(false);
-                break;
-            case R.id.tv_topic_by_me:
-                mAdapter.clear();
-                for (int i = 0; i < 4; i++) {
-                    Topic topic = new Topic();
-                    topic.setTopicContent("大家的宝宝都是几个月会说话的" + i);
-                    topic.setTopicName("飞飞的妈妈：#大家的宝宝都是几个月会说话的？#" + i);
-                    topic.setTopicReplyNum(String.valueOf(i));
-                    mAdapter.add(topic);
-                }
-                tvTopicByHot.setSelected(false);
-                tvTopicByMe.setSelected(true);
-                break;
+        if (v == tvTopicByHot) {
+            moveLineFrom = moveLine.getX();
+            moveLineTo =moveLineFrom - moveLine.getWidth();
+            ObjectAnimator moveLeftLineAnim = ObjectAnimator.ofFloat(moveLine, "translationX", moveLineFrom, moveLineTo);
+            moveLeftLineAnim.setDuration(300);
+            moveLeftLineAnim.start();
+
+            mAdapter.clear();
+            for (int i = 0; i < 10; i++) {
+                Topic topic = new Topic();
+                topic.setTopicContent("RayRay的爸爸：#食物中含有硫酸锌？酸奶？#" + i);
+                topic.setTopicName("什么食物中含有硫酸锌？" + i);
+                topic.setTopicReplyNum(String.valueOf(i));
+                mAdapter.add(topic);
+            }
+            tvTopicByHot.setSelected(true);
+            tvTopicByMe.setSelected(false);
+        } else if (v == tvTopicByMe) {
+            moveLineFrom = moveLine.getX();
+            moveLineTo = moveLineFrom + moveLine.getWidth();
+            ObjectAnimator moveRightLineAnim = ObjectAnimator.ofFloat(moveLine, "translationX", moveLineFrom, moveLineTo);
+            moveRightLineAnim.setDuration(300);
+            moveRightLineAnim.start();
+
+            mAdapter.clear();
+            for (int i = 0; i < 4; i++) {
+                Topic topic = new Topic();
+                topic.setTopicContent("飞飞的妈妈：#大家的宝宝都是几个月会说话的？#" + i);
+                topic.setTopicName("大家的宝宝都是几个月会说话的" + i);
+                topic.setTopicReplyNum(String.valueOf(i));
+                mAdapter.add(topic);
+            }
+            tvTopicByHot.setSelected(false);
+            tvTopicByMe.setSelected(true);
         }
     }
 }
