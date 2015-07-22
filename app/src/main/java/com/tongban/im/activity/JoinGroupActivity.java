0100398337a692736a0ed8bc336d9b1077de401d
ray <always_0122@163.com>
2015-07-22 10:05:57 +0800

    修改strings，提取公用部分

---
+++ app/src/main/java/com/tongban/im/activity/JoinGroupActivity.java
@@ -0,0 +1,64 @@
+package com.tongban.im.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.tongban.im.R;
+import com.tongban.im.activity.base.BaseToolBarActivity;
+
+/**
+ * 加入圈子
+ *
+ * @author zhangleilei
+ * @createTime 2015/07/22
+ */
+public class JoinGroupActivity extends BaseToolBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_join_group;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_join_group, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
