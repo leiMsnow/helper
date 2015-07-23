325602b6c6d58c9e185c27387b78223bed4c7283
fushudi <fushudi@dangdang.com>
2015-07-23 10:56:01 +0800

    修改创建圈子UI

--- app/src/main/java/com/tongban/im/activity/CreateGroupActivity.java
+++ app/src/main/java/com/tongban/im/activity/CreateGroupActivity.java
@@ -1,5 +1,9 @@
 package com.tongban.im.activity;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
 
@@ -8,7 +12,8 @@ import com.tongban.im.activity.base.BaseToolBarActivity;
  *
  * @author fushudi
  */
-public class CreateGroupActivity extends BaseToolBarActivity {
+public class CreateGroupActivity extends BaseToolBarActivity implements View.OnClickListener {
+    private TextView tvSetGroupIcon;
 
     @Override
     protected int getLayoutRes() {
@@ -18,16 +23,22 @@ public class CreateGroupActivity extends BaseToolBarActivity {
 
     @Override
     protected void initView() {
-
+        tvSetGroupIcon = (TextView) findViewById(R.id.tv_add_group_icon);
     }
 
     @Override
     protected void initListener() {
-
+        tvSetGroupIcon.setOnClickListener(this);
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == tvSetGroupIcon) {
+        }
     }
 }
