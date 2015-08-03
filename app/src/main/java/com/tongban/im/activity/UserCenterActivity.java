8a36d5211535aa791a8a6a3819f3fb47c5c63e5b
ray <always_0122@163.com>
2015-08-03 18:05:11 +0800

    增加用户中心

--- app/src/main/java/com/tongban/im/activity/UserCenterActivity.java
+++ app/src/main/java/com/tongban/im/activity/UserCenterActivity.java
@@ -2,9 +2,13 @@ package com.tongban.im.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.tongban.corelib.utils.DensityUtils;
+import com.tongban.corelib.utils.ScreenUtils;
+import com.tongban.corelib.widget.view.ptz.PullToZoomListViewEx;
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
 
@@ -18,7 +22,7 @@ import in.srain.cube.views.ptr.header.StoreHouseHeader;
  */
 public class UserCenterActivity extends BaseToolBarActivity {
 
-    private ListView lvUserCenter;
+    private PullToZoomListViewEx lvUserCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,41 +31,26 @@ public class UserCenterActivity extends BaseToolBarActivity {
 
     @Override
     protected int getLayoutRes() {
+
         return R.layout.activity_user_center;
     }
 
     @Override
     protected void initView() {
-        lvUserCenter = (ListView) findViewById(R.id.lv_user_center);
-        final PtrFrameLayout ptrFrameLayout = (PtrFrameLayout) findViewById(R.id.ptr_frame);
-        StoreHouseHeader header = new StoreHouseHeader(mContext);
-        header.setPadding(0, DensityUtils.dp2px(mContext,20), 0, DensityUtils.dp2px(mContext,20));
-        header.initWithString("Ultra PTR");
-
-        ptrFrameLayout.setDurationToCloseHeader(1500);
-        ptrFrameLayout.setHeaderView(header);
-        ptrFrameLayout.addPtrUIHandler(header);
-        ptrFrameLayout.setPtrHandler(new PtrHandler() {
-            @Override
-            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
-                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
-            }
-
-            @Override
-            public void onRefreshBegin(PtrFrameLayout frame) {
-                ptrFrameLayout.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        ptrFrameLayout.refreshComplete();
-                    }
-                }, 1500);
-            }
-        });
+        lvUserCenter = (PullToZoomListViewEx) findViewById(R.id.lv_user_center);
     }
 
     @Override
     protected void initData() {
+        String[] adapterData = new String[]{"Activity", "Service", "Content Provider", "Intent", "BroadcastReceiver", "ADT", "Sqlite3", "HttpClient",
+                "DDMS", "Android Studio", "Fragment", "Loader", "Activity", "Service", "Content Provider", "Intent",
+                "BroadcastReceiver", "ADT", "Sqlite3", "HttpClient", "Activity", "Service", "Content Provider", "Intent",
+                "BroadcastReceiver", "ADT", "Sqlite3", "HttpClient"};
 
+        lvUserCenter.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, adapterData));
+        int mScreenWidth = ScreenUtils.getScreenWidth(mContext);
+        AbsListView.LayoutParams localObject = new AbsListView.LayoutParams(mScreenWidth, (int) (9.0F * (mScreenWidth / 16.0F)));
+        lvUserCenter.setHeaderLayoutParams(localObject);
     }
 
     @Override
