6e72dcec67cd0362d5d74df58b1fe5c9c0577835
chenenyu <chenenyu@dangdang.com>
2015-08-08 13:19:17 +0800

    添加浮动按钮,修改创建圈子的的入口

--- app/src/main/java/com/tongban/im/fragment/CircleFragment.java
+++ app/src/main/java/com/tongban/im/fragment/CircleFragment.java
@@ -1,7 +1,6 @@
 package com.tongban.im.fragment;
 
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,8 +11,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.tongban.corelib.base.fragment.BaseApiFragment;
+import com.tongban.corelib.utils.ToastUtil;
 import com.tongban.im.R;
-import com.tongban.im.activity.ChooseGroupTypeActivity;
 import com.tongban.im.model.BaseEvent;
 
 import io.rong.imkit.fragment.ConversationListFragment;
@@ -71,9 +70,8 @@ public class CircleFragment extends BaseApiFragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_create_group) {
-            Intent intent = new Intent(mContext, ChooseGroupTypeActivity.class);
-            startActivity(intent);
+        if (item.getItemId() == R.id.action_search) {
+            ToastUtil.getInstance(mContext).showToast("搜索");
             return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/tongban/im/fragment/RecommendCircleFragment.java
+++ app/src/main/java/com/tongban/im/fragment/RecommendCircleFragment.java
@@ -1,20 +1,21 @@
 package com.tongban.im.fragment;
 
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.SearchView.OnQueryTextListener;
-import android.text.TextUtils;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 import android.widget.ListView;
 
 import com.tongban.corelib.base.fragment.BaseApiFragment;
 import com.tongban.im.R;
+import com.tongban.im.activity.ChooseGroupTypeActivity;
 
 /**
  * 推荐圈子的Fragment
  * Created by Cheney on 15/8/3.
  */
-public class RecommendCircleFragment extends BaseApiFragment implements OnQueryTextListener {
-    private SearchView mSearchView;
+public class RecommendCircleFragment extends BaseApiFragment {
     private ListView mListView;
+    private FloatingActionButton mFab;
 
     @Override
     protected int getLayoutRes() {
@@ -23,13 +24,19 @@ public class RecommendCircleFragment extends BaseApiFragment implements OnQueryT
 
     @Override
     protected void initView() {
-        mSearchView = (SearchView) mView.findViewById(R.id.search_view);
         mListView = (ListView) mView.findViewById(R.id.lv_circle);
+        mFab = (FloatingActionButton) mView.findViewById(R.id.fab_add);
     }
 
     @Override
     protected void initListener() {
-        mSearchView.setOnQueryTextListener(this);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, ChooseGroupTypeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -37,16 +44,4 @@ public class RecommendCircleFragment extends BaseApiFragment implements OnQueryT
 
     }
 
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        if (!TextUtils.isEmpty(query)) {
-            // cey 搜索
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        return false;
-    }
 }
