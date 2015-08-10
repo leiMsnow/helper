499940542eb9d4be762fcaa6084c361fa84b1bc3
ray <always_0122@163.com>
2015-08-10 18:39:09 +0800

    增加话题搜索

--- app/src/main/java/com/tongban/im/activity/SearchTopicActivity.java
+++ app/src/main/java/com/tongban/im/activity/SearchTopicActivity.java
@@ -1,6 +1,5 @@
 package com.tongban.im.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
