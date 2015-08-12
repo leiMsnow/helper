9b7547be2d212f80b29b480efc7afd057c833a30
fushudi <fushudi@dangdang.com>
2015-08-12 13:38:33 +0800

    官方话题评论 - UI修改

--- app/src/main/java/com/tongban/im/activity/AuthorityTopicDetailsActivity.java
+++ app/src/main/java/com/tongban/im/activity/AuthorityTopicDetailsActivity.java
@@ -27,7 +27,7 @@ public class AuthorityTopicDetailsActivity extends BaseToolBarActivity {
 
     @Override
     protected int getLayoutRes() {
-        return R.layout.activity_authority_topic_detail;
+        return R.layout.activity_authority_topic_details;
     }
 
     @Override
@@ -40,45 +40,59 @@ public class AuthorityTopicDetailsActivity extends BaseToolBarActivity {
     @Override
     protected void initData() {
         List<AuthorityTopic> authorityTopicList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            AuthorityTopic authorityTopic = new AuthorityTopic();
-            Product product = new Product();
-            TopicReply topicReply = new TopicReply();
-            Topic topic = new Topic();
-            if (i % 3 == 0) {
-                authorityTopic.setContentType(AuthorityTopic.CONTENT);
 
-                product.setProductAdvantage("该产品的优势是舒适、环保，该产品的优势是舒适、环保该产品的" +
-                        "优势是舒适、环保，该产品的优势是舒适、环保该产品的优势是舒适、环保" + i);
-                product.setProductCollectNum("3" + i);
-                product.setProductDisAdvantage("无明显不足" + i);
-                product.setProductIntroduction("该产品属于当当的产品，产于1987年。该产品属于当当的产品，" +
-                        "产于1987年，该产品属于当当的产品，产于1987年。该产品属于当当的产品，" +
-                        "产于1987年该产品属于当当的产品，产于1987年。");
-                product.setProductName("澳大利亚品牌");
-                product.setProductParameter("175ml；产地：澳大利亚");
-            } else if (i % 3 == 1) {
-                authorityTopic.setContentType(AuthorityTopic.REPLY);
-                topicReply.setPortrait_url("http://g.hiphotos.baidu.com/image/pic/item/77c6a7efce1b9d16633a4168f1deb48f8c54643e.jpg");
-                topicReply.setReplyNickName("小鹿妈妈");
-                topicReply.setReplyContent("该产品属于当当的产品，产于1987年。该产于1987年，该产品属于当当的产品，产于1987年。");
-                topicReply.setReplyTime("19:13");
-            } else if (i % 3 == 2) {
-                authorityTopic.setContentType(AuthorityTopic.REPLY_NUM);
-                topic.setTopicTime("19:09");
-                topic.setTopicPraiseNum("90" + i);
-                topic.setTopicName("宝宝专用");
-            }
+        //产品相关
+        AuthorityTopic authorityTopic = new AuthorityTopic();
+        Product product = new Product();
+        authorityTopic.setContentType(AuthorityTopic.CONTENT);
+        product.setProductAdvantage("该产品的优势是舒适、环保，该产品的优势是舒适、环保该产品的" +
+                "优势是舒适、环保，该产品的优势是舒适、环保该产品的优势是舒适、环保");
+        product.setProductCollectNum("3");
+        product.setProductDisAdvantage("无明显不足");
+        product.setProductIntroduction("该产品属于当当的产品，产于1987年。该产品属于当当的产品，" +
+                "产于1987年，该产品属于当当的产品，产于1987年。该产品属于当当的产品，" +
+                "产于1987年该产品属于当当的产品，产于1987年。");
+        product.setProductName("澳大利亚品牌");
+        product.setProductParameter("175ml；产地：澳大利亚");
+        product.setProduct_img_url("http://g.hiphotos.baidu.com/image/pic/item/77c6a7efce1b9d16633a4168f1deb48f8c54643e.jpg");
+        product.setProduct_icon_url("http://g.hiphotos.baidu.com/image/pic/item/77c6a7efce1b9d16633a4168f1deb48f8c54643e.jpg");
+        authorityTopic.setProduct(product);
+        authorityTopicList.add(authorityTopic);
+
+        //数量（评论、点赞）相关
+        authorityTopic = new AuthorityTopic();
+        Topic topic = new Topic();
+        authorityTopic.setContentType(AuthorityTopic.REPLY_NUM);
+        topic.setTopicPraiseNum("90");
+        topic.setTopicReplyNum("100");
+        authorityTopic.setTopic(topic);
+        authorityTopicList.add(authorityTopic);
 
 
-            authorityTopic.setProduct(product);
-            authorityTopic.setTopic(topic);
-            authorityTopic.setTopicReply(topicReply);
-            authorityTopicList.add(authorityTopic);
-        }
+        //评论相关
+        authorityTopic = new AuthorityTopic();
+        TopicReply topicReply = new TopicReply();
+        authorityTopic.setContentType(AuthorityTopic.REPLY);
+        topicReply.setPortrait_url("http://g.hiphotos.baidu.com/image/pic/item/77c6a7efce1b9d16633a4168f1deb48f8c54643e.jpg");
+        topicReply.setReplyNickName("小鹿妈妈");
+        topicReply.setReplyContent("该产品属于当当的产品，产于1987年。该产于1987年，该产品属于当当的产品，产于1987年。");
+        topicReply.setReplyTime("19:13");
+        authorityTopic.setTopicReply(topicReply);
+        authorityTopicList.add(authorityTopic);
+
+        authorityTopic = new AuthorityTopic();
+        topicReply = new TopicReply();
+        authorityTopic.setContentType(AuthorityTopic.REPLY);
+        topicReply.setPortrait_url("http://g.hiphotos.baidu.com/image/pic/item/77c6a7efce1b9d16633a4168f1deb48f8c54643e.jpg");
+        topicReply.setReplyNickName("小鹿妈妈");
+        topicReply.setReplyContent("该产品属于当当的产品，产于1987年。该产于1987年，该产品属于当当的产品，产于1987年。");
+        topicReply.setReplyTime("19:13");
+        authorityTopic.setTopicReply(topicReply);
+        authorityTopicList.add(authorityTopic);
+
         mAdapter = new AuthorityTopicDetailsAdapter(mContext, authorityTopicList);
         lvAuthorityTopicDetails.addHeaderView(LayoutInflater.from(mContext).
-                inflate(R.layout.activity_authority_topic_detail_header, null));
+                inflate(R.layout.activity_authority_topic_details_header, null));
         lvAuthorityTopicDetails.setAdapter(mAdapter);
 
     }
--- app/src/main/java/com/tongban/im/adapter/CreateTopicImgAdapter.java
+++ app/src/main/java/com/tongban/im/adapter/CreateTopicImgAdapter.java
@@ -3,10 +3,13 @@ package com.tongban.im.adapter;
 import android.content.Context;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.tongban.corelib.base.adapter.BaseAdapterHelper;
 import com.tongban.corelib.base.adapter.QuickAdapter;
 import com.tongban.im.R;
+import com.tongban.im.utils.CameraUtils;
+import com.tongban.im.widget.view.AlertView;
 
 import java.util.List;
 
@@ -15,6 +18,9 @@ import java.util.List;
  * Created by fushudi on 2015/7/16.
  */
 public class CreateTopicImgAdapter extends QuickAdapter<String> {
+    private AlertView dialog;
+    private LinearLayout mCamera;
+    private LinearLayout mGallery;
 
     public CreateTopicImgAdapter(Context context, int layoutResId, List data) {
         super(context, layoutResId, data);
@@ -30,14 +36,39 @@ public class CreateTopicImgAdapter extends QuickAdapter<String> {
         helper.setOnClickListener(R.id.iv_topic_img, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                createDialog();
+//                if (TextUtils.isEmpty(item) && mData.size() <= 2) {
+//                    add(0, "http://img2.3lian.com/2014/f7/5/d/22.jpg");
+//
+//                } else if (TextUtils.isEmpty(item) && mData.indexOf(item) == 2) {
+//                    //替换
+//                    set(mData.indexOf(item), "http://www.touxiang.cn/uploads/20120723/23-033215_282.jpg");
+//                }
+            }
 
-                if (TextUtils.isEmpty(item) && mData.size() <= 2) {
-                    add(0, "http://img2.3lian.com/2014/f7/5/d/22.jpg");
+            // 打开相机的提示框
+            private void createDialog() {
+                if (dialog == null) {
+                    dialog = new AlertView(mContext);
+                    mCamera = (LinearLayout) dialog.findViewById(R.id.camera);
+                    mGallery = (LinearLayout) dialog.findViewById(R.id.gallery);
 
-                } else if (TextUtils.isEmpty(item) && mData.indexOf(item) == 2) {
-                    //替换
-                    set(mData.indexOf(item), "http://www.touxiang.cn/uploads/20120723/23-033215_282.jpg");
+                    mCamera.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            CameraUtils.takePhoto(mContext);
+                            dialog.cancel();
+                        }
+                    });
+                    mGallery.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            CameraUtils.openPhotoAlbum(mContext);
+                            dialog.cancel();
+                        }
+                    });
                 }
+                dialog.show();
             }
         });
     }
