83ff8cb917bdfadc00c98df241f37cbafcd9eca8
fushudi <fushudi@dangdang.com>
2015-08-15 14:34:01 +0800

    增加粉丝关注界面

--- app/src/main/java/com/tongban/im/activity/PersonalDataActivity.java
+++ app/src/main/java/com/tongban/im/activity/PersonalDataActivity.java
@@ -1,6 +1,10 @@
 package com.tongban.im.activity;
 
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
 
@@ -25,5 +29,20 @@ public class PersonalDataActivity extends BaseToolBarActivity {
     @Override
     protected void initListener() {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_personal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.fans) {
+            startActivity(new Intent(this,MyInfoActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
