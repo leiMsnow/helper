a0f2759cad8ae8fda1ab0fbda5a4fdf77e7eb04b
fushudi <fushudi@dangdang.com>
2015-09-01 17:25:51 +0800

    非空判断

--- app/src/main/java/com/tongban/im/fragment/user/InputChildInfoFragment.java
+++ app/src/main/java/com/tongban/im/fragment/user/InputChildInfoFragment.java
@@ -1,11 +1,19 @@
 package com.tongban.im.fragment.user;
 
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
 import com.tongban.corelib.base.fragment.BaseApiFragment;
 import com.tongban.im.R;
 
 
-public class InputChildInfoFragment extends BaseApiFragment {
+public class InputChildInfoFragment extends BaseApiFragment implements TextWatcher {
+    private EditText etChildNickName, etChildBirthday;
+
+    private String mChildNickName, mChildBirthday;
 
     @Override
     protected int getLayoutRes() {
@@ -14,16 +22,35 @@ public class InputChildInfoFragment extends BaseApiFragment {
 
     @Override
     protected void initView() {
-
+        etChildNickName = (EditText) mView.findViewById(R.id.et_input_nickname);
+        etChildBirthday = (EditText) mView.findViewById(R.id.et_input_birthday);
     }
 
     @Override
     protected void initData() {
+        if (TextUtils.isEmpty(mChildBirthday) || TextUtils.isEmpty(mChildNickName)) {
 
+        }
     }
 
     @Override
     protected void initListener() {
+        etChildBirthday.addTextChangedListener(this);
+        etChildNickName.addTextChangedListener(this);
+    }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        mChildNickName = etChildNickName.getText().toString().trim();
+        mChildBirthday = etChildBirthday.getText().toString().trim();
     }
 }
