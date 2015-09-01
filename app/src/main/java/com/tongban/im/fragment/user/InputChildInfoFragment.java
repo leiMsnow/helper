1185f037a29ccb7f82c9786cd38c8842b727cf19
fushudi <fushudi@dangdang.com>
2015-09-01 11:36:09 +0800

    填写孩子信息界面

---
+++ app/src/main/java/com/tongban/im/fragment/user/InputChildInfoFragment.java
@@ -0,0 +1,29 @@
+package com.tongban.im.fragment.user;
+
+
+import com.tongban.corelib.base.fragment.BaseApiFragment;
+import com.tongban.im.R;
+
+
+public class InputChildInfoFragment extends BaseApiFragment {
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.fragment_input_child_info;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
