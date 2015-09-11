0489b3915a41205dbb93ec05050e900a5eae07a6
fushudi <fushudi@dangdang.com>
2015-09-11 10:48:09 +0800

    修改个人资料

--- app/src/main/java/com/tongban/im/activity/user/PersonalInfoActivity.java
+++ app/src/main/java/com/tongban/im/activity/user/PersonalInfoActivity.java
@@ -1,28 +1,43 @@
 package com.tongban.im.activity.user;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.tongban.im.R;
 import com.tongban.im.activity.base.BaseToolBarActivity;
+import com.tongban.im.activity.base.CameraResultActivity;
+import com.tongban.im.api.FileUploadApi;
+import com.tongban.im.api.UploadFileCallback;
 import com.tongban.im.api.UserCenterApi;
 import com.tongban.im.model.BaseEvent;
+import com.tongban.im.model.EditUser;
+import com.tongban.im.model.ImageUrl;
 import com.tongban.im.model.User;
+import com.tongban.im.widget.view.CameraView;
 
 /**
  * 个人资料界面
  *
  * @author fushudi
  */
-public class PersonalInfoActivity extends BaseToolBarActivity implements View.OnClickListener {
+public class PersonalInfoActivity extends CameraResultActivity implements View.OnClickListener,
+        CameraResultActivity.IPhotoListener {
+    private LinearLayout llUpdateUserPortrait;
     private ImageView ivUserIcon;
     private TextView tvNickName, tvPhoneNum, tvChildAge,
-            tvChildSex, tvChildConstellation, tvChildSchool, tvAddress;
+            tvChildSex, tvChildConstellation, tvChildSchool;
 
     private User user;
+    private CameraView mCameraView;
+    private EditUser editUser = new EditUser();
+
+    private byte[] mIcon;
 
     @Override
     protected int getLayoutRes() {
@@ -32,6 +47,7 @@ public class PersonalInfoActivity extends BaseToolBarActivity implements View.On
     @Override
     protected void initView() {
         setTitle("个人资料");
+        llUpdateUserPortrait = (LinearLayout) findViewById(R.id.ll_update_user_portrait);
         ivUserIcon = (ImageView) findViewById(R.id.iv_user_icon);
         tvNickName = (TextView) findViewById(R.id.tv_user_name);
         tvPhoneNum = (TextView) findViewById(R.id.tv_phone_num);
@@ -39,7 +55,6 @@ public class PersonalInfoActivity extends BaseToolBarActivity implements View.On
         tvChildSex = (TextView) findViewById(R.id.tv_child_sex);
         tvChildConstellation = (TextView) findViewById(R.id.tv_child_constellation);
         tvChildSchool = (TextView) findViewById(R.id.tv_chile_school);
-        tvAddress = (TextView) findViewById(R.id.tv_address);
 
     }
 
@@ -50,23 +65,24 @@ public class PersonalInfoActivity extends BaseToolBarActivity implements View.On
 
     @Override
     protected void initListener() {
-        ivUserIcon.setOnClickListener(this);
+        setmPhotoListener(this);
+
+        llUpdateUserPortrait.setOnClickListener(this);
         tvNickName.setOnClickListener(this);
         tvPhoneNum.setOnClickListener(this);
         tvChildAge.setOnClickListener(this);
         tvChildSex.setOnClickListener(this);
         tvChildConstellation.setOnClickListener(this);
         tvChildSchool.setOnClickListener(this);
-        tvAddress.setOnClickListener(this);
 
     }
 
     //返回个人资料数据
     public void onEventMainThread(BaseEvent.UserInfoEvent obj) {
         this.user = obj.user;
-        if (user.getPortrait_url()!=null){
+        if (user.getPortrait_url() != null) {
             Glide.with(mContext).load(user.getPortrait_url().getMin()).into(ivUserIcon);
-        }else {
+        } else {
             ivUserIcon.setImageResource(R.drawable.rc_default_portrait);
         }
         tvNickName.setText(user.getNick_name());
@@ -77,11 +93,50 @@ public class PersonalInfoActivity extends BaseToolBarActivity implements View.On
             tvChildConstellation.setText(user.getChild_info().get(0).getConstellation());
             tvChildSchool.setText(user.getChild_info().get(0).getSchool());
         }
-        tvAddress.setText(user.getAddress());
     }
 
     @Override
     public void onClick(View v) {
+        //修改头像
+        if (v == llUpdateUserPortrait) {
+            createDialog();
 
+        }
+    }
+
+    // 打开相机的提示框
+    private void createDialog() {
+        if (mCameraView == null) {
+            mCameraView = new CameraView(mContext);
+        }
+        mCameraView.show();
+    }
+
+    @Override
+    public void sendPhoto(byte[] bytes) {
+        mIcon = bytes;
+        FileUploadApi.getInstance().uploadFile(mIcon, null, FileUploadApi.IMAGE_SIZE_300,
+                FileUploadApi.IMAGE_SIZE_500, new UploadFileCallback() {
+
+                    @Override
+                    public void uploadSuccess(ImageUrl url) {
+                        editUser.setPortrait_url(url);
+                        updateUser();
+                    }
+
+                    @Override
+                    public void uploadFailed(String error) {
+                        updateUser();
+                    }
+
+                });
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        ivUserIcon.setImageBitmap(bitmap);
+        UserCenterApi.getInstance().fetchPersonalCenterInfo(this);
+    }
+
+    private void updateUser() {
+        UserCenterApi.getInstance().updateUserInfo(editUser, PersonalInfoActivity.this);
     }
 }
