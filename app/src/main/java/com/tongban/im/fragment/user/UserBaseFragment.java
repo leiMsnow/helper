b736936465e9d5421ae7bb5f15e034c571556750
fushudi <fushudi@dangdang.com>
2015-10-28 14:36:17 +0800

    个人中心toolbar

--- app/src/main/java/com/tongban/im/fragment/user/UserBaseFragment.java
+++ app/src/main/java/com/tongban/im/fragment/user/UserBaseFragment.java
@@ -1,12 +1,9 @@
 package com.tongban.im.fragment.user;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.NestedScrollView;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,14 +13,8 @@ import android.widget.TextView;
 
 import com.tb.api.model.user.User;
 import com.tb.api.utils.TransferCenter;
-import com.tongban.corelib.utils.AnimatorUtils;
 import com.tongban.corelib.utils.SPUtils;
-import com.tongban.corelib.utils.ScreenUtils;
 import com.tongban.corelib.widget.view.CircleImageView;
-import com.tongban.corelib.widget.view.indicator.CirclePageIndicator;
-import com.tongban.corelib.widget.view.ptz.PullToZoomBase;
-import com.tongban.corelib.widget.view.ptz.PullToZoomScrollViewEx;
-import com.tongban.corelib.widget.view.transformer.ScalePageTransformer;
 import com.tongban.im.R;
 import com.tongban.im.activity.user.MyInfoActivity;
 import com.tongban.im.adapter.UserInfoAdapter;
@@ -31,7 +22,6 @@ import com.tongban.im.common.Consts;
 import com.tongban.im.fragment.base.BaseToolBarFragment;
 
 import butterknife.Bind;
-import butterknife.OnClick;
 
 public class UserBaseFragment extends BaseToolBarFragment implements View.OnClickListener {
     @Bind(R.id.sv_user_center)
@@ -43,41 +33,37 @@ public class UserBaseFragment extends BaseToolBarFragment implements View.OnClic
 //    @Bind(R.id.iv_close)
 //    ImageView ivClose;
     //headerView
-//    @Bind(R.id.iv_user_portrait)
+    @Bind(R.id.iv_user_portrait)
     CircleImageView ivUserPortrait;
-    //    @Bind(R.id.vp_container)
-    ViewPager vpChildInfo;
-    //    @Bind(R.id.lpi_indicator)
-    CirclePageIndicator indicator;
 
-    //    @Bind(R.id.rl_action_parent)
+    @Bind(R.id.rl_action_parent)
     RelativeLayout rlActionParent;
-    //    @Bind(R.id.tv_follow_num)
+    @Bind(R.id.tv_follow_num)
     protected TextView tvFollowCount;
-    //    @Bind(R.id.rl_follow_num)
+    @Bind(R.id.rl_follow_num)
     LinearLayout rlFollowNum;
-    //    @Bind(R.id.tv_fans_num)
+    @Bind(R.id.tv_fans_num)
     TextView tvFansCount;
-    //    @Bind(R.id.rl_fans_num)
+    @Bind(R.id.rl_fans_num)
     LinearLayout rlFansNum;
-    //    @Bind(R.id.tv_group_num)
+    @Bind(R.id.tv_group_num)
     TextView tvGroupCount;
-    //    @Bind(R.id.rl_group_num)
+    @Bind(R.id.rl_group_num)
     LinearLayout rlGroupNum;
-    //    @Bind(R.id.ll_relationship)
+    @Bind(R.id.ll_relationship)
     LinearLayout vHeaderBottom;
     // contentView
-//    @Bind(R.id.tv_my_topic)
+    @Bind(R.id.tv_my_topic)
     protected TextView tvMyTopic;
-    //    @Bind(R.id.tv_my_collect)
+    @Bind(R.id.tv_my_collect)
     protected TextView tvMyCollect;
-    //    @Bind(R.id.tv_settings)
+    @Bind(R.id.tv_settings)
     protected TextView tvSettings;
-    //    @Bind(R.id.iv_focus)
+    @Bind(R.id.iv_focus)
     protected Button ivFocus;
-    //    @Bind(R.id.iv_cancel_focus)
+    @Bind(R.id.iv_cancel_focus)
     protected Button ivCancelFocus;
-    //    @Bind(R.id.iv_private_chat)
+    @Bind(R.id.iv_private_chat)
     protected Button ivPrivateChat;
 
     //zoomView
@@ -99,80 +85,9 @@ public class UserBaseFragment extends BaseToolBarFragment implements View.OnClic
 
     @Override
     protected void initData() {
-
-        headView = LayoutInflater.from(mContext).inflate(R.layout.ptz_head_view_personal_center, null, false);
-        zoomView = LayoutInflater.from(mContext).inflate(R.layout.ptz_zoom_view, null, false);
-        contentView = LayoutInflater.from(mContext).inflate(R.layout.ptz_content_view, null, false);
-
-//        lvUserCenter.setHeaderView(headView);
-//        lvUserCenter.setZoomView(zoomView);
-//        lvUserCenter.setScrollContentView(contentView);
-
-        lvUserCenter.addView(headView);
-//        lvUserCenter.addView(zoomView);
-//        lvUserCenter.addView(contentView);
-
-        //headView
-        ivUserPortrait = (CircleImageView) headView.findViewById(R.id.iv_user_portrait);
-        vpChildInfo = (ViewPager) headView.findViewById(R.id.vp_container);
-        rlActionParent = (RelativeLayout) getActivity().findViewById(R.id.rl_action_parent);
-        indicator = (CirclePageIndicator) headView.findViewById(R.id.lpi_indicator);
-        vHeaderBottom = (LinearLayout) headView.findViewById(R.id.ll_relationship);
-        tvFansCount = (TextView) headView.findViewById(R.id.tv_fans_num);
-        tvFollowCount = (TextView) headView.findViewById(R.id.tv_follow_num);
-        tvGroupCount = (TextView) headView.findViewById(R.id.tv_group_num);
-        rlFansNum = (LinearLayout) headView.findViewById(R.id.rl_fans_num);
-        rlFollowNum = (LinearLayout) headView.findViewById(R.id.rl_follow_num);
-        rlGroupNum = (LinearLayout) headView.findViewById(R.id.rl_group_num);
-        ivFocus = (Button) headView.findViewById(R.id.iv_focus);
-        ivPrivateChat = (Button) headView.findViewById(R.id.iv_private_chat);
-        ivCancelFocus = (Button) headView.findViewById(R.id.iv_cancel_focus);
-        // contentView
-        tvMyTopic = (TextView) contentView.findViewById(R.id.tv_my_topic);
-        tvMyCollect = (TextView) contentView.findViewById(R.id.tv_my_collect);
-        tvSettings = (TextView) contentView.findViewById(R.id.tv_settings);
-        // zoomView
-        ivZoomTop = (ImageView) zoomView.findViewById(R.id.iv_zoom_top);
-
         rlFansNum.setEnabled(false);
         rlFollowNum.setEnabled(false);
         rlGroupNum.setEnabled(false);
-
-        int mScreenWidth = ScreenUtils.getScreenWidth(mContext);
-        LinearLayout.LayoutParams localObject = new LinearLayout.LayoutParams(mScreenWidth,
-                (int) (3.0F * (mScreenWidth / 4.0F)));
-//        lvUserCenter.setHeaderLayoutParams(localObject);
-//
-//        lvUserCenter.setOnPullZoomListener(new PullToZoomBase.OnPullZoomListener() {
-//            @Override
-//            public void onPullZooming(int newScrollValue) {
-//                float scrollValue = -newScrollValue;
-//                float headerBottomHeight = vHeaderBottom.getHeight() * 2;
-//                float startValue = alphaValue;
-//                if (scrollValue < headerBottomHeight) {
-//                    alphaValue = (headerBottomHeight - scrollValue) / headerBottomHeight;
-//
-//                    AnimatorUtils.animatorToAlpha(ivZoomTop, startValue, alphaValue, 10);
-//                    AnimatorUtils.animatorToAlpha(ivUserPortrait, startValue, alphaValue, 10);
-//                    AnimatorUtils.animatorToAlpha(vpChildInfo, startValue, alphaValue, 10);
-//                    AnimatorUtils.animatorToAlpha(rlActionParent, startValue, alphaValue, 10);
-//                }
-//            }
-//
-//            @Override
-//            public void onPullZoomEnd() {
-//                AnimatorUtils.animatorToAlpha(ivZoomTop, alphaValue, 1.0f, 300);
-//                AnimatorUtils.animatorToAlpha(ivUserPortrait, alphaValue, 1.0f, 300);
-//                AnimatorUtils.animatorToAlpha(vpChildInfo, alphaValue, 1.0f, 300);
-//                AnimatorUtils.animatorToAlpha(rlActionParent, alphaValue, 1.0f, 300);
-//                new Handler().postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        alphaValue = 1.0f;
-//                    }
-//                }, 300);
-//            }
-//        });
 
         ivUserPortrait.setOnClickListener(this);
 
@@ -228,13 +143,6 @@ public class UserBaseFragment extends BaseToolBarFragment implements View.OnClic
             int resId = (Integer) SPUtils.
                     get(mContext, SPUtils.NO_CLEAR_FILE, Consts.KEY_DEFAULT_PORTRAIT, 0);
             ivUserPortrait.setImageResource(resId);
-        }
-        if (mUserInfo.getChildInfo() != null &&
-                mUserInfo.getChildInfo().size() > 0) {
-            mAdapter = new UserInfoAdapter(mContext, mUserInfo.getChildInfo(), mUserInfo.getUser_id());
-            vpChildInfo.setAdapter(mAdapter);
-            indicator.setViewPager(vpChildInfo);
-            vpChildInfo.setPageTransformer(true, new ScalePageTransformer());
         }
         tvFansCount.setText(String.valueOf(mUserInfo.getFans_amount()));
         tvFollowCount.setText(String.valueOf(mUserInfo.getFocused_amount()));
